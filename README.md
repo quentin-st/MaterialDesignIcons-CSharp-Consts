b97dc90feafa77c5befc80e4671257242065f5df
Quentin Stoeckel <chteuchteu@gmail.com>
2016-07-19 14:01:33 +0200

    Start from MaterialDesignIcons-SCSS-Variables, update it & enjoy

---
+++ README.md
@@ -0,0 +1,24 @@
+# MaterialDesignIcons-CSharp-Consts
+This SCSS part file allows you to use [MaterialDesignIcons](https://github.com/Templarian/MaterialDesign)
+project directly from your C# code: benefit from autocomplete and avoid typos.
+
+![MaterialDesignIcons-CSharp-Consts](doc/screenshot.gif)
+
+## How to use this
+Copy [`generated/MaterialDesignIcons.cs`](https://raw.githubusercontent.com/chteuchteu/MaterialDesignIcons-CSharp-Consts/master/generated/MaterialDesignIcons.cs)
+into your project, customize its namespace (`MyProject.Helpers`) and start using it!
+
+## How to update `MaterialDesignIcons.cs` file
+
+> Note: you usually don't want to do that, except if some icons are missing from the generated file.
+
+The `generate.py` script generates `MaterialDesignIcons.cs` from MaterialDesignIcons input file:
+
+    public const string AccessPoint = "mdi-access-point";
+    public const string AccessPointNetwork = "mdi-access-point-network";
+    public const string Account = "mdi-account";
+    public const string AccountAlert = "mdi-account-alert";
+
+1. Run `python generate.py`: [_variables.scss](https://raw.githubusercontent.com/Templarian/MaterialDesign-Webfont/master/scss/_variables.scss)
+will be downloaded and parsed from MaterialDesignIcon's repository to update `MaterialDesignIcons.cs`.
+2. Enjoy `MaterialDesignIcons.cs`!
---
+++ generate.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib.request
+import re
+
+icon_prefix = 'mdi-'
+input_file_uri = 'https://raw.githubusercontent.com/Templarian/MaterialDesign-Webfont/master/scss/_variables.scss'
+output_file = 'generated/MaterialDesignIcons.cs'
+output_prefix = """namespace MyProject.Helpers
+{
+    /**
+     * MaterialDesignIcons-C#-Consts
+     * https://github.com/chteuchteu/MaterialDesignIcons-CSharp-Consts
+     */
+    public abstract class Mdi
+    {
+"""
+
+output_sufix = """    }
+}
+"""
+
+# Download & parse input file
+response = urllib.request.urlopen(input_file_uri)
+data = response.read()
+input = data.decode('utf-8')
+
+regex = re.compile('    "(.*)": (F[A-F0-9]*),?')
+matches = regex.findall(input)
+
+if len(matches) == 0:
+    print('Could not find variables.')
+    sys.exit(1)
+
+# Write in generated file
+with open(output_file, 'w') as output:
+    output.truncate()
+
+    output.writelines(output_prefix)
+
+    for match in matches:
+        icon_name = match[0]
+
+        # Remove dashes, capitalize
+        var_name = icon_name.replace('-', ' ').title().replace(' ', '')
+
+        output.write('        public const string {} = "{}";\n'.format(var_name, icon_prefix + icon_name))
+
+    output.writelines(output_sufix)
+
+    print("Generated {} with {} variables".format(output_file, len(matches)))
